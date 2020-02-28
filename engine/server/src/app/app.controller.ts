d3ffea207fb8db08ae8d898b75a69f285bc64636
hyp6xia <hyp6xia@gmail.com>
2020-02-28 02:35:07 +0000

    Added ping route

--- engine/server/src/app/app.controller.ts
+++ engine/server/src/app/app.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller('app')
-export class AppController {}
+export class AppController {
+
+  /**
+   * App controller constructor.
+   * @version 1.0.0
+   */
+  constructor() {}
+  
+  /**
+   * Used for accepting requests for account creation.
+   * @version 1.0.0
+   * @param {CreateAccountDTO} dto - data transfer object containing account info.
+   */
+  @Get('ping')
+  @HttpCode(202)
+  async ping() {}
+}
