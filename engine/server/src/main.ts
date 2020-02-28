ad8b2d820442175d9158a18ae5f97e53c5e97337
hyp6xia <hyp6xia@gmail.com>
2020-02-28 18:29:44 +0000

    Updated server longs (for init run)

--- engine/server/src/main.ts
+++ engine/server/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
 
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
+  console.log(`[EE] Server running on port ${process.env.PORT || 3000}`)
 }
 bootstrap();
