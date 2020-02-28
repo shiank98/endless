45a9aac327a3066eb4ecd7c6c68170c7f4c90194
hyp6xia <hyp6xia@gmail.com>
2020-02-28 17:31:02 +0000

    Added client pings to logs

--- engine/client/server.js
+++ engine/client/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/client'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function(req, res) {
+  console.log(`[EC] Received a ping at ${Date.now()}!`)
   res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
 
